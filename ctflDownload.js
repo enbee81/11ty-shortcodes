c80efc48af36190975eb06a2617b9fd7273f2aad
Nils Binder <nils.binder@icloud.com>
2022-11-23 13:23:32 +0100

    add better description and more properties

--- ctflDownload.js
+++ ctflDownload.js
@@ -1,14 +1,26 @@
-// This takes a file from a contentful media-input field and caches it locally.
-// The file is then written to dist/downloads using the file's title as filename.
-// A link to the file is returned.
+/* ==========================================================
+This shortcode takes a file from a contentful media-input field and caches it locally.
+The file is then written to dist/downloads using the file's id as filename.
+It returns a link to the downloaded file.
+
+Required properties:
+  - downloadObj -> The whole file object from contentful (not just the URL!)
+
+Optional properties:
+  - title -> will be used as filename for the download HTML-attribute
+  - classes -> list of classes that is applied to the link
+========================================================== */
 
 const EleventyFetch = require("@11ty/eleventy-fetch");
 const fs = require("fs");
 
-async function ctflDownloadShortcode(ctflDownload) {
-  let url = "https:" + ctflDownload.downloadObj.fields.file.url;
-  let filetype = url.split(".").pop();
-  let filename = ctflDownload.downloadObj.fields.title;
+async function ctflDownloadShortcode(content, ctflDownload) {
+  const url = "https:" + ctflDownload.downloadObj.fields.file.url;
+  const filetype = url.split(".").pop();
+  const filename = ctflDownload.downloadObj.sys.id;
+  const title = ctflDownload.title ? ctflDownload.title : filename;
+  const classes = ctflDownload.classes ? ctflDownload.classes : "";
+
   let data = await EleventyFetch(url, {
     duration: "1d",
     directory: ".cache",
@@ -31,7 +43,7 @@ async function ctflDownloadShortcode(ctflDownload) {
     console.log("Done writing " + filename); // Success
   });
 
-  return `<a href="/downloads/${filename}.${filetype}" download>${filename}</a>`;
+  return `<a class="${classes}" href="/downloads/${filename}.${filetype}" download="${title}">${content}</a>`;
 }
 
 module.exports = ctflDownloadShortcode;
